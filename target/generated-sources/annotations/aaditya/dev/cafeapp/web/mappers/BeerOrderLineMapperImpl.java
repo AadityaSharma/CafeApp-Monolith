a863e0d65bbfd574cb530ef5bb35fb6bbc2d17b7
Aaditya Sharma <accolite-asharma@evernote.com>
2020-10-26 22:56:36 +0530

    Project initial setup

---
+++ target/generated-sources/annotations/aaditya/dev/cafeapp/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,15 @@
+package aaditya.dev.cafeapp.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-10-26T22:19:41+0530",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+}
